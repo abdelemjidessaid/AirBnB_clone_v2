e6d95c46e5a5653ec01e3e558152295f15dcebec
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-09-17 12:01:03 +0100

    task 4 is created and pushed up.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,11 +3,11 @@
 -- to the created database, and flush the changes.
 
 -- create database named 'hbnb_test_db' if not exists.
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create user named 'hbnb_test' at 'localhost' if not exists.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant 'hbnb_test' all privileges on 'hbnb_test_db'.
-GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- flush privileges.
 FLUSH PRIVILEGES;
 -- grant SELECT privileges on 'performance_schema' database to user 'hbnb_test'.
